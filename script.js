f85414ad9b58ce5b865402e32a22169c726f7922
visi0nz <civas.em@gmail.com>
2025-04-20 21:40:26 +0200

    Updated
    website is now responsive

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
+// Hamburger Menu
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+const menu = document.querySelector(".menu-links");
+const icon = document.querySelector(".hamburger-icon");
+menu.classList.toggle("open");
 }
-
-
-// disappearing Navbar
-
-  const navbar = document.getElementById("desktop-nav");
-  let lastScrollY = window.scrollY;
-
-  window.addEventListener("scroll", () => {
-    const currentScrollY = window.scrollY;
-
-    if (currentScrollY > lastScrollY && currentScrollY > 100) {
-      navbar.style.transform = "translateY(-100%)";
-    } else {
-      navbar.style.transform = "translateY(0)";
-    }
-    lastScrollY = currentScrollY;
-  });
-
 
 // Language Switcher
 function switchLanguage(lang) {
