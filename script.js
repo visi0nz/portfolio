20200368a1a831a411a3b34bb2724126dc14c72d
visi0nz <civas.em@gmail.com>
2025-04-23 21:25:19 +0200

    Updated

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function switchLanguage(lang) {
       window.location.href = 'index.html'; 
   }
 }
+
+
+// Navbar
+let lastScrollY = window.scrollY; // Track the last scroll position
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header"); // Select the header element
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        header.classList.add("hidden");
+    } else {
+        // Scrolling up
+        header.classList.remove("hidden");
+    }
+    lastScrollY = window.scrollY; // Update the last scroll position
+});
