b501c3b9120a0b6f11f4bf209849303772e61277
visi0nz <civas.em@gmail.com>
2025-09-06 14:35:41 +0200

    index change

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ menu.classList.toggle("open");
 // Language Switcher
 function switchLanguage(lang) {
   if (lang === 'de') {
-      window.location.href = 'indexger.html'; 
-  } else {
       window.location.href = 'index.html'; 
+  } else {
+      window.location.href = 'index-eng.html'; 
   }
 }
 
